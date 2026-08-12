26631f6011c8fc078a74e6b1d2861d25b5a5efe5
Raj Kumar <shahraj0312@gmail.com>
2026-08-12 08:26:39 +0530

    refactor: add connector registry

---
+++ backend/src/connectors/registry.ts
@@ -0,0 +1,9 @@
+import type { PlatformConnector } from "../../../connectors/common/connector.js";
+import { MockPlatformConnector } from "./mock_connector.js";
+
+export const activeConnectors: PlatformConnector[] = [
+  new MockPlatformConnector("flipkart", "Flipkart", 999, "IN_STOCK", "Tomorrow"),
+  new MockPlatformConnector("amazon", "Amazon", 1029, "IN_STOCK", "Tomorrow"),
+  new MockPlatformConnector("zepto", "Zepto", 1049, "IN_STOCK", "12 mins"),
+  new MockPlatformConnector("blinkit", "Blinkit", null, "OUT_OF_STOCK", undefined),
+];
